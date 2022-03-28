ef66322d22c8e11913cc9bee62a6aeeba1cfd16f
M3MONs <67465063+M3MONs@users.noreply.github.com>
2022-03-28 14:41:15 +0200

    utworzone strone Home

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import HeaderMenu from "components/organisms/HeaderMenu/HeaderMenu";
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <HeaderMenu />
+        </>
+    );
+};
+
+export default Home;
