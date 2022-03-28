40fe7abbe79f7b265f82cfd457a75dabc8556fbe
M3MONs <67465063+M3MONs@users.noreply.github.com>
2022-03-28 14:42:04 +0200

    Dodano strone Home do App

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -3,12 +3,19 @@ import { Container } from "@material-ui/core";
 import styled, { ThemeProvider } from "styled-components";
 import { theme } from "assets/styles/theme";
 import NavBar from "components/organisms/NavBar/NavBar";
+import Home from "./Home";
 
 const Wrapper = styled.div`
-    background-color: ${({ theme }) => theme.colors.lightBlue};
+    /* background-color: ${({ theme }) => theme.colors.lightBlue}; */
     min-height: 100vh;
     width: 100%;
     overflow-x: hidden;
+`;
+
+const StyledContainer = styled(Container)`
+    && {
+        padding: 0px;
+    }
 `;
 
 function App() {
@@ -18,9 +25,9 @@ function App() {
             <ThemeProvider theme={theme}>
                 <Wrapper>
                     <NavBar />
-                    <Container>
-                        <h1>Blank Page</h1>
-                    </Container>
+                    <StyledContainer minwidth='sm'>
+                        <Home />
+                    </StyledContainer>
                 </Wrapper>
             </ThemeProvider>
         </>
