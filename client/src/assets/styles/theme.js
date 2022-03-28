de221d060eab713f72c090d52406cd9816745546
M3MONs <67465063+M3MONs@users.noreply.github.com>
2022-03-28 14:38:50 +0200

    zmiana koloru darkBlue

--- client/src/assets/styles/theme.js
+++ client/src/assets/styles/theme.js
@@ -1,11 +1,12 @@
 export const theme = {
     colors: {
         lightBlue: "#A6D8D4",
-        darkBlue: "#3C91E6",
+        darkBlue: "#2e51a2",
         darkestBlue: "#185D78",
         orange: "#FE5F00",
         black: "#000000",
         gunMetal: "#2F323A",
         pink: "#FFDBDA",
+        white: "#FFFFFF",
     },
 };
