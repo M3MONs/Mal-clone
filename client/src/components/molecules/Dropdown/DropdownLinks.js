adcbd7fce7db07a0132906dfaaa62365a2314cb1
M3MONs <alan.skrrrz@gmail.com>
2022-03-30 15:29:46 +0200

    wyeksportowano "css" do osobnego pliku

--- client/src/components/molecules/Dropdown/DropdownLinks.js
+++ client/src/components/molecules/Dropdown/DropdownLinks.js
@@ -1,16 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import { LinksList } from "./Dropdown.style";
 import DropdownLink from "components/atoms/DropdownLink/DropdownLink";
-
-const LinksList = styled.ul`
-    background-color: ${({ theme }) => theme.colors.darkWhite};
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    min-width: 118px;
-    list-style: none;
-`;
 
 const DropdownLinks = ({ links }) => {
     return (
