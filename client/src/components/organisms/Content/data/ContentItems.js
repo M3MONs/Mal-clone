3fb183323ba8a4ffb86091e95b70842e67920e4d
M3MONs <alan.skrrrz@gmail.com>
2022-03-30 21:22:20 +0200

    Zmieniono tabele dla wszystkich widgetow

---
+++ client/src/components/organisms/Content/data/ContentItems.js
@@ -0,0 +1,68 @@
+import EventLink from "components/atoms/EventLink/EventLink";
+import SeasonAnime from "components/atoms/SeasonAnime/SeasonAnime";
+
+export const Widgets = [
+    {
+        title: "MALxJapan -More than just anime-",
+        linkTitle: "Visit MALxJapan",
+        link: "https://mxj.myanimelist.net/",
+        slider: false,
+        Atom: EventLink,
+        items: [
+            {
+                img: "https://cdn.myanimelist.net/images/mxj/20220325/bp.png",
+                title: "Blue Period Art Contest merchandise is now available for pre-order! 🎨",
+                link: "https://mxj.myanimelist.net/bpart/?utm_source=MAL&utm_medium=top-mxj&utm_content=bpart",
+            },
+            {
+                img: "https://cdn.myanimelist.net/images/mxj/20220325/furuba.jpg",
+                title: "Tohru and co. take the stage in Fruits Basket the Stage! Get your tickets 🍙",
+                link: "https://mxj.myanimelist.net/m-fruba/?utm_source=MAL&utm_medium=top-mxj&utm_content=m-fruba",
+            },
+            {
+                img: "https://cdn.myanimelist.net/images/mxj/20220328/nbcu.png",
+                title: "Vote for your favorite NBCUniversal in the Anime General Election! 🗳",
+                link: "https://mxj.myanimelist.net/nbcuniversal_animexmusic30thproject/generalelection_anime/?utm_source=MAL&utm_medium=top-mxj&utm_content=nbcu",
+            },
+        ],
+    },
+    {
+        title: "Spring 2022 Anime",
+        linkTitle: "View More",
+        link: "https://myanimelist.net/anime/season",
+        Atom: SeasonAnime,
+        slider: true,
+        items: [
+            {
+                img: "https://cdn.myanimelist.net/r/320x440/images/anime/1109/118948.webp?s=11cc1052e67c3c522310dabd46e84507",
+                title: "Rikei ga Koi ni Ochita no de Shoumei shitemita. Heart",
+                link: "https://myanimelist.net/anime/43470/Rikei_ga_Koi_ni_Ochita_no_de_Shoumei_shitemita_Heart",
+            },
+            {
+                img: "https://cdn.myanimelist.net/r/320x440/images/anime/1247/121186.webp?s=c92b3f65b9964e4a742f41dc416b6a71",
+                title: "Honzuki no Gekokujou: Shisho ni Naru Tame ni wa Shudan wo Erandeiraremasen 3rd Season",
+                link: "https://myanimelist.net/anime/42429/Honzuki_no_Gekokujou__Shisho_ni_Naru_Tame_ni_wa_Shudan_wo_Erandeiraremasen_3rd_Season",
+            },
+            {
+                img: "https://cdn.myanimelist.net/r/160x220/images/anime/1361/120706.webp?s=478b92258494352f4d9a2b5a94f85f65",
+                title: "Gaikotsu Kishi-sama, Tadaima Isekai e Odekakechuu",
+                link: "https://myanimelist.net/anime/48760/Gaikotsu_Kishi-sama_Tadaima_Isekai_e_Odekakechuu",
+            },
+            {
+                img: "https://cdn.myanimelist.net/r/320x440/images/anime/1201/121039.webp?s=86974485010837642bbc52278acc984a",
+                title: "Yuusha, Yamemasu",
+                link: "https://myanimelist.net/anime/50175/Yuusha_Yamemasu",
+            },
+            {
+                img: "https://cdn.myanimelist.net/r/320x440/images/anime/1120/120796.webp?s=a7675286ee945ae127a3e51ac86b75d8",
+                title: "Summertime Render",
+                link: "https://myanimelist.net/anime/47194/Summertime_Render",
+            },
+            {
+                img: "https://cdn.myanimelist.net/r/320x440/images/anime/1247/121186.webp?s=c92b3f65b9964e4a742f41dc416b6a71",
+                title: "Rikei ga Koi ni Ochita no de Shoumei shitemita. Heart",
+                link: "https://myanimelist.net/anime/42429/Honzuki_no_Gekokujou__Shisho_ni_Naru_Tame_ni_wa_Shudan_wo_Erandeiraremasen_3rd_Season",
+            },
+        ],
+    },
+];
