126f3faf9270213aee509c6fe6a4affb7341b557
M3MONs <alan.skrrrz@gmail.com>
2022-03-30 16:09:20 +0200

    Utworzono Wrappery pod Content

--- client/src/components/organisms/Content/Content.js
+++ client/src/components/organisms/Content/Content.js
@@ -1,11 +1,38 @@
-import HelloMessage from "components/atoms/HelloMessage/HelloMessage";
 import React from "react";
+import styled from "styled-components";
+import HelloMessage from "components/atoms/HelloMessage/HelloMessage";
+import Widget from "components/molecules/Widget/Widget";
+
+const Wrapper = styled.div`
+    padding: 10px 0;
+    border-left: 1px solid ${({ theme }) => theme.colors.gray};
+    border-right: 1px solid ${({ theme }) => theme.colors.gray};
+`;
+
+const LeftWrapper = styled.div`
+    width: 780px;
+    border-right: 1px solid ${({ theme }) => theme.colors.gray};
+`;
+
+const RightWrapper = styled.div``;
 
 const Content = () => {
     return (
-        <div>
+        <>
             <HelloMessage />
-        </div>
+            <Wrapper>
+                {/* Lewa Kolumna */}
+                <LeftWrapper>
+                    <Widget
+                        title='MALxJapan -More than just anime-'
+                        linkTitle='Visit MALxJapan'
+                        link='https://mxj.myanimelist.net/'
+                    ></Widget>
+                </LeftWrapper>
+                {/* Prawa Kolumna */}
+                <RightWrapper></RightWrapper>
+            </Wrapper>
+        </>
     );
 };
 
