c1a72a2223e95db9828c36af91bb339b58315d64
M3MONs <alan.skrrrz@gmail.com>
2022-03-30 21:23:32 +0200

    PropTypes + "css" + dodanie slidera dla widgetow

--- client/src/components/molecules/Widget/Widget.js
+++ client/src/components/molecules/Widget/Widget.js
@@ -1,5 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
+import Slider from "../Slider/Slider";
 
 const Wrapper = styled.article`
     margin: 0 10px 20px;
@@ -9,7 +11,7 @@ const Header = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    border-bottom: 1px solid ${({ theme }) => theme.colors.gray};
+    border-bottom: 1px solid ${({ theme }) => theme.colors.darkGray};
     h2,
     a {
         font-size: 12px;
@@ -17,13 +19,17 @@ const Header = styled.div`
         text-decoration: none;
     }
     a {
-        color: #1d439b;
+        color: ${({ theme }) => theme.colors.linkBlue};
     }
 `;
 
-const Content = styled.div``;
+const Content = styled.div`
+    display: ${(props) => (props.slider ? "block" : "flex")};
+    justify-content: space-between;
+    margin: 5px 0 20px;
+`;
 
-const Widget = ({ children, title, linkTitle, link }) => {
+const Widget = ({ children, title, linkTitle, link, slider = false }) => {
     return (
         <Wrapper>
             <Header>
@@ -32,9 +38,16 @@ const Widget = ({ children, title, linkTitle, link }) => {
                     {linkTitle}
                 </a>
             </Header>
-            <Content>{children}</Content>
+            <Content slider={slider}>{slider ? <Slider>{children}</Slider> : children}</Content>
         </Wrapper>
     );
+};
+
+Widget.propTypes = {
+    title: PropTypes.string,
+    linkTitle: PropTypes.string,
+    link: PropTypes.string,
+    slider: PropTypes.bool,
 };
 
 export default Widget;
