2692c7fc11f1af2482022f54aa34fc611af7a1b9
M3MONs <alan.skrrrz@gmail.com>
2022-03-30 15:28:06 +0200

    zmiana lokalizacji pliku

---
+++ client/src/components/molecules/SearchBar/data/SelectItems.js
@@ -0,0 +1,14 @@
+export const SelectItems = [
+    "All",
+    "Anime",
+    "Manga",
+    "Characters",
+    "People",
+    "Companies",
+    "Manga Store",
+    "News",
+    "Featured Articles",
+    "Forum",
+    "Clubs",
+    "Users",
+];
