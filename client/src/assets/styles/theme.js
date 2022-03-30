e4fcace32bed5ed91673558f619f32ad7b231d34
M3MONs <alan.skrrrz@gmail.com>
2022-03-30 21:19:58 +0200

    dodano kolor linkBlue oraz darkGray

--- client/src/assets/styles/theme.js
+++ client/src/assets/styles/theme.js
@@ -1,6 +1,7 @@
 export const theme = {
     colors: {
         lightBlue: "#e1e7f5",
+        linkBlue: "#1d439b",
         darkBlue: "#2e51a2",
         darkestBlue: "#185D78",
         orange: "#FE5F00",
@@ -9,6 +10,7 @@ export const theme = {
         pink: "#FFDBDA",
         darkWhite: "#E1E7F5",
         gray: "#ebebeb",
+        darkGray: "#bebebe",
         white: "#FFFFFF",
     },
 };
