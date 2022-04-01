e38a534daecbf2b234cd01524f9e4c6f0ddc0bfc
M3MONs <alan.skrrrz@gmail.com>
2022-04-01 12:43:06 +0200

    osobny plik dla 'css' + parametr slides

--- client/src/components/molecules/Widget/Widget.js
+++ client/src/components/molecules/Widget/Widget.js
@@ -1,35 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import { Wrapper, Header, Content } from "./Widget.style";
 import Slider from "../Slider/Slider";
 
-const Wrapper = styled.article`
-    margin: 0 10px 20px;
-`;
-
-const Header = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid ${({ theme }) => theme.colors.darkGray};
-    h2,
-    a {
-        font-size: 12px;
-        padding: 5px 0;
-        text-decoration: none;
-    }
-    a {
-        color: ${({ theme }) => theme.colors.linkBlue};
-    }
-`;
-
-const Content = styled.div`
-    display: ${(props) => (props.slider ? "block" : "flex")};
-    justify-content: space-between;
-    margin: 5px 0 20px;
-`;
-
-const Widget = ({ children, title, linkTitle, link, slider = false }) => {
+const Widget = ({ children, title, linkTitle, link, slider = false, slides = null }) => {
     return (
         <Wrapper>
             <Header>
@@ -38,7 +12,9 @@ const Widget = ({ children, title, linkTitle, link, slider = false }) => {
                     {linkTitle}
                 </a>
             </Header>
-            <Content slider={slider}>{slider ? <Slider>{children}</Slider> : children}</Content>
+            <Content slider={slider}>
+                {slider ? <Slider slides={slides}>{children}</Slider> : children}
+            </Content>
         </Wrapper>
     );
 };
---
+++ client/src/components/molecules/Widget/Widget.style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.article`
+    margin: 0 10px 20px;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.darkGray};
+    h2,
+    a {
+        font-size: 12px;
+        padding: 5px 0;
+        text-decoration: none;
+    }
+    a {
+        color: ${({ theme }) => theme.colors.linkBlue};
+    }
+`;
+
+export const Content = styled.div`
+    display: ${(props) => (props.slider ? "block" : "flex")};
+    justify-content: space-between;
+    margin: 5px 0 20px;
+`;
