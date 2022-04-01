7f32e9c8af21609ef110cebdb14dc7a909e60ca7
M3MONs <alan.skrrrz@gmail.com>
2022-04-01 15:08:08 +0200

    utworzono atom DefaultLink

---
+++ client/src/components/atoms/DefaultLink/DefaultLink.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const DefaultLink = styled.a`
+    font-size: 11px;
+    color: ${({ theme }) => theme.colors.linkBlue};
+    font-weight: ${(props) => (props.bold ? "bold" : "normal")};
+    z-index: 20;
+    cursor: pointer;
+    text-decoration: none;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    &:hover {
+        text-decoration: underline;
+    }
+`;
