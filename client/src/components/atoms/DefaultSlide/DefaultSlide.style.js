6785b1a9766d3fa9059ce767b3a0876c64827508
M3MONs <alan.skrrrz@gmail.com>
2022-04-01 15:07:36 +0200

    SliderLink=>DefaultSlide

---
+++ client/src/components/atoms/DefaultSlide/DefaultSlide.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    height: 177px;
+    img {
+        width: 108px;
+        height: 163px;
+    }
+    .text {
+        width: 108px;
+        position: absolute;
+        bottom: 0px;
+    }
+`;
