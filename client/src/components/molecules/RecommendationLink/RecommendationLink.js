f74f8e2ad6b43d4d5eadb9f77abfe651389b36fa
M3MONs <alan.skrrrz@gmail.com>
2022-04-06 13:51:01 +0200

    zaktualizowano pod atom RecommendationCell

---
+++ client/src/components/molecules/RecommendationLink/RecommendationLink.js
@@ -0,0 +1,22 @@
+import React from "react";
+import RecommendationCell from "components/atoms/RecommendationCell/RecommendationCell";
+import { Wrapper } from "./RecommendationLink.style";
+
+const RecommendationLink = ({ item: { text, recom, first, second } }) => {
+    return (
+        <Wrapper>
+            <table cellPadding={0} cellSpacing={0} className='anime-wrapper'>
+                <tbody>
+                    <tr>
+                        <RecommendationCell anime={first} first={true} />
+                        <RecommendationCell anime={second} />
+                    </tr>
+                </tbody>
+            </table>
+            <div>{text}</div>
+            <div className='recommendation' dangerouslySetInnerHTML={{ __html: recom }}></div>
+        </Wrapper>
+    );
+};
+
+export default RecommendationLink;
