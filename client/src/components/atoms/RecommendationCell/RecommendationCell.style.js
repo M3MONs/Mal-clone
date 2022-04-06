4a1aaa0936ca4ebfe06e2970e1443011d0ea5cad
M3MONs <alan.skrrrz@gmail.com>
2022-04-06 13:48:55 +0200

    Atom pojedynczej komorki dla rekomendacji

---
+++ client/src/components/atoms/RecommendationCell/RecommendationCell.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Cell = styled.td`
+    display: table-cell;
+    line-height: 1.75;
+    width: 50%;
+    .anime {
+        display: flex;
+        gap: 10px;
+    }
+`;
