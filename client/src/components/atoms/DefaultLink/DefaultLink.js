21fef8f50a82ac2dd28b94b4a8b0d5e2f759b37b
M3MONs <alan.skrrrz@gmail.com>
2022-04-08 16:21:19 +0200

    ustawienie font-size,white-space w propsie

--- client/src/components/atoms/DefaultLink/DefaultLink.js
+++ client/src/components/atoms/DefaultLink/DefaultLink.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const DefaultLink = styled.a`
-    font-size: 12px;
+    font-size: ${(props) => (props.size ? props.size : "12px")};
     color: ${({ theme }) => theme.colors.linkBlue};
     font-weight: ${(props) => (props.bold ? "bold" : "normal")};
     z-index: 20;
@@ -9,7 +9,7 @@ export const DefaultLink = styled.a`
     text-decoration: none;
     overflow: hidden;
     text-overflow: ellipsis;
-    white-space: nowrap;
+    white-space: ${(props) => (props.wrap ? "wrap" : "nowrap")};
     &:hover {
         text-decoration: underline;
     }
