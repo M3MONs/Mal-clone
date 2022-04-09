99cf8d9687309dc94e12f2f71191288f937b7646
M3MONs <alan.skrrrz@gmail.com>
2022-04-09 19:58:19 +0200

    usunieto niepotrzebny css

--- client/src/components/organisms/NavBar/NavBar.js
+++ client/src/components/organisms/NavBar/NavBar.js
@@ -3,22 +3,6 @@ import { Wrapper, NavWrapper } from "./NavBar.style";
 import NavLinks from "components/molecules/NavLinks/NavLinks";
 import Logo from "components/atoms/Logo/Logo";
 
-const Wrapper = styled.nav`
-    background-color: white;
-    width: 100%;
-    height: 50px;
-`;
-
-const NavWrapper = styled(Container)`
-    padding: 0 !important;
-    && {
-        height: 100%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-`;
-
 const NavBar = () => {
     return (
         <Wrapper>
