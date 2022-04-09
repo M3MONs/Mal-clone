fa46c74918fab6ffce1d19d204d4b19d296e1110
M3MONs <alan.skrrrz@gmail.com>
2022-04-09 19:27:54 +0200

    molecule dla linkow w footerze

---
+++ client/src/components/molecules/FooterLinks/FooterLinks.js
@@ -0,0 +1,36 @@
+import { SocialsIcon } from "components/atoms/SocialsIcon/SocialsIcon";
+import React from "react";
+import { Wrapper, Block } from "./FooterLinks.style";
+
+const FooterLinks = ({ firstBlock = {}, secondBlock = null }) => {
+    return (
+        <Wrapper>
+            {firstBlock && (
+                <Block>
+                    {firstBlock?.title && <span>{firstBlock.title}</span>}
+                    <div className='icons'>
+                        {firstBlock.items.map((i) => {
+                            return i.link ? (
+                                <SocialsIcon href={i.link}>{i.atom}</SocialsIcon>
+                            ) : (
+                                <>{i.atom}</>
+                            );
+                        })}
+                    </div>
+                </Block>
+            )}
+            {secondBlock && (
+                <Block>
+                    <span>{secondBlock.title}</span>
+                    <div className='icons'>
+                        {secondBlock.items.map((i) => (
+                            <SocialsIcon href={i.link}>{i.atom}</SocialsIcon>
+                        ))}
+                    </div>
+                </Block>
+            )}
+        </Wrapper>
+    );
+};
+
+export default FooterLinks;
