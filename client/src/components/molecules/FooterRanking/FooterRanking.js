948169fef13a73511980db1548da5a4eed7e8535
M3MONs <alan.skrrrz@gmail.com>
2022-04-09 19:28:34 +0200

    molecule+atom+data dla rankingow w footerze

---
+++ client/src/components/molecules/FooterRanking/FooterRanking.js
@@ -0,0 +1,35 @@
+import { DefaultLink } from "components/atoms/DefaultLink/DefaultLink";
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: 200px;
+`;
+
+const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.gray};
+    padding: 5px 0;
+    h3 {
+        font-weight: normal;
+        font-size: 14px;
+    }
+`;
+
+const Content = styled.ul``;
+
+const FooterRanking = ({ children = null, link = "/#", title = "" }) => {
+    return (
+        <Wrapper>
+            <Header>
+                <h3>{title}</h3>
+                <DefaultLink href={link}>More</DefaultLink>
+            </Header>
+            <Content>{children}</Content>
+        </Wrapper>
+    );
+};
+
+export default FooterRanking;
