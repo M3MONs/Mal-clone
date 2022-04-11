29263b264d9396725ebc2729b84f86ca7e34e573
M3MONs <alan.skrrrz@gmail.com>
2022-04-11 15:44:25 +0200

    usuniecie importu HelloMessage

--- client/src/components/organisms/Content/Content.js
+++ client/src/components/organisms/Content/Content.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import HelloMessage from "components/atoms/HelloMessage/HelloMessage";
 import Widget from "../Widget/Widget";
 
 import { SwiperSlide } from "swiper/react";
