a5cb02163b910664341ca24dc57023268e5efcd4
M3MONs <alan.skrrrz@gmail.com>
2022-04-16 15:21:32 +0200

    usunieto font loga

--- client/src/components/atoms/Logo/Logo.js
+++ client/src/components/atoms/Logo/Logo.js
@@ -2,8 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.h1`
-    @import url("https://fonts.googleapis.com/css2?family=Akaya+Telivigala&display=swap");
-    font-family: "Akaya Telivigala", cursive;
     color: ${({ theme }) => theme.colors.darkBlue};
     font-size: 2rem;
 `;
