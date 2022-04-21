7b95835dfa31037e63b91edf0fd64121222c6e51
M3MONs <alan.skrrrz@gmail.com>
2022-04-21 16:20:16 +0200

    pobieranie + wyswietlanie danych z api

--- client/src/components/molecules/RightWidget/RightWidget.js
+++ client/src/components/molecules/RightWidget/RightWidget.js
@@ -1,8 +1,26 @@
 import { DefaultLink } from "components/atoms/Links/DefaultLink/DefaultLink";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import RightWidgetItem from "components/atoms/Links/RightWidgetLink/RightWidgetLink";
 import { Wrapper, Header, ContentWrapper } from "./RightWidget.style";
 
 const RightWidget = ({ widget = {}, children }) => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        // Pobieranie danych z serwera
+        const fetchData = async () => {
+            try {
+                const { data } = await axios.post(widget.apiLink);
+                setData(data);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        // Jezeli nie ma widget.apiLink to nie wykonujemy
+        if (widget?.apiLink) fetchData();
+    }, []);
+
     return (
         <Wrapper>
             <Header>
@@ -11,7 +29,9 @@ const RightWidget = ({ widget = {}, children }) => {
                     More
                 </DefaultLink>
             </Header>
-            <ContentWrapper>{children}</ContentWrapper>
+            <ContentWrapper>
+                {data && data.map((item) => <RightWidgetItem item={item} />)}
+            </ContentWrapper>
         </Wrapper>
     );
 };
